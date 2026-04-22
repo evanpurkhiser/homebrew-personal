025e35f2d12ce0ee5870087062ef5df01f41a704
Evan Purkhiser <evanpurkhiser@gmail.com>
2026-04-22 11:44:55 -0400

    iptorrents-cli: point at versioned tag archive

--- iptorrents-cli.rb
+++ iptorrents-cli.rb
@@ -1,9 +1,8 @@
 class IptorrentsCli < Formula
   desc "CLI for searching and downloading torrents from IPTorrents"
   homepage "https://github.com/evanpurkhiser/iptorrents-cli"
-  url "https://github.com/evanpurkhiser/iptorrents-cli/archive/refs/heads/main.tar.gz"
-  version "0.1.0"
-  sha256 "496dfc51eee8904ab989e5e667f92bea830c134d249181a1db7c7fba3470b364"
+  url "https://github.com/evanpurkhiser/iptorrents-cli/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "e42035d6be3092f4c8a3f82f14ffaaa0cc46a3647b6d01688bee7ef74a92fb3f"
   license "MIT"
 
   head "https://github.com/evanpurkhiser/iptorrents-cli.git", branch: "main"
