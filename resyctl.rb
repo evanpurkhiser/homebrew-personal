616644a3452aa3f2cc89f9f0ab342498514dddf7
Evan Purkhiser <evanpurkhiser@gmail.com>
2026-04-25 00:42:30 -0400

    Add resyctl formula

---
+++ resyctl.rb
@@ -0,0 +1,20 @@
+class Resyctl < Formula
+  desc "Automation-friendly CLI for Resy with JSON output"
+  homepage "https://github.com/evanpurkhiser/resyctl"
+  url "https://github.com/evanpurkhiser/resyctl/archive/refs/tags/v0.0.0.tar.gz"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  license "MIT"
+
+  head "https://github.com/evanpurkhiser/resyctl.git", branch: "main"
+
+  depends_on arch: :arm64
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "auth", shell_output("#{bin}/resyctl --help")
+  end
+end
